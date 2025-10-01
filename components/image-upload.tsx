5136bedcc26c0a1622bfb99f02b4c9c7a50854db
Nahom77 <nahomtigistu777@gmail.com>
2025-10-01 11:30:24 +0300

    uploadthing setted up

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UploadDropzone } from "@/lib/uploadthing";
+import { UploadDropzone } from "../lib/uploadthing";
 import { XIcon } from "lucide-react";
 
 interface ImageUploadProps {
@@ -12,14 +12,18 @@ interface ImageUploadProps {
 function ImageUpload({ endpoint, onChange, value }: ImageUploadProps) {
   if (value) {
     return (
-      <div className="relative size-40">
-        <img src={value} alt="Upload" className="rounded-md size-40 object-cover" />
+      <div className="size-40 relative">
+        <img
+          src={value}
+          alt="Upload"
+          className="size-40 object-cover rounded-md"
+        />
         <button
           onClick={() => onChange("")}
-          className="absolute top-0 right-0 p-1 bg-red-500 rounded-full shadow-sm"
+          className="p-1 top-0 right-0 absolute bg-red-500 shadow-sm rounded-full"
           type="button"
         >
-          <XIcon className="h-4 w-4 text-white" />
+          <XIcon className="w-4 h-4 text-white" />
         </button>
       </div>
     );
